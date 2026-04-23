664042bd436ab558e39103b797a7321f441d1cb8
ESTUDIANTE <ESTUDIANTE@DESKTOP-5CLN2LU>
2026-04-23 18:41:18 -0500

    Desarrollo de clase biblioteca y los metodos para agregar prestamos, categorias y autores

--- src/main/java/org/buber/modelos/Biblioteca.java
+++ src/main/java/org/buber/modelos/Biblioteca.java
@@ -1,4 +1,48 @@
 package org.buber.modelos;
+import java.util.ArrayList;
 
 public class Biblioteca {
+    
+    private ArrayList<Categoria> categorias;
+    private ArrayList<Autor> autores;
+    private ArrayList<Prestamo> prestamos;
+    
+  
+    public Biblioteca(){
+        categorias = new ArrayList<>();
+        autores = new ArrayList<>();
+        prestamos = new ArrayList<>();
+    }
+
+    public ArrayList<Autor> getAutores() {
+        return autores;
+    }
+
+    public ArrayList<Categoria> getCategoria(){
+        return categorias;
+    }
+    
+    public ArrayList<Prestamo> getPrestamo(){
+        return prestamos;
+    }
+    
+    @Override
+    public String toString() {
+        return "Biblioteca{" + "categorias=" + categorias + ", autores=" + autores + ", prestamos=" + prestamos + '}';
+    }
+    
+    public void agregarCategoria( Categoria categoria){
+        categorias.add(categoria);
+    }
+    
+    public void agregarAutor( Autor autor){
+        autores.add(autor);
+    }
+    
+    public void agregarPrestamo( Prestamo prestamo){
+        prestamos.add(prestamo);
+    }    
+    
+    
+    
 }
