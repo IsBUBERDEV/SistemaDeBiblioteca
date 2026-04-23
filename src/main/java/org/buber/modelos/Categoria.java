9b7f5c7fd27e60313abd0b80c5c53abe9d7b8e22
Jp <jeprimera16@gmail.com>
2026-04-23 18:34:21 -0500

    commit de clases "Autor" y "Categoria"

--- src/main/java/org/buber/modelos/Categoria.java
+++ src/main/java/org/buber/modelos/Categoria.java
@@ -1,4 +1,35 @@
 package org.buber.modelos;
+import java.util.ArrayList;
 
 public class Categoria {
+    private final int id;
+    private final String nombre;
+    private ArrayList<Libro> listaLibros;
+
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.listaLibros = new ArrayList<>();
+    }
+    
+    public void Agregarlibro(Libro libro){
+        this.listaLibros.add(libro);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Libro> getListaLibros() {
+        return listaLibros;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nombre=" + nombre + ", listaLibros=" + listaLibros + '}';
+    }   
 }
