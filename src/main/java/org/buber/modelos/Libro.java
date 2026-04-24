ee9e06d7e3ab33d65ba52c6f521c8e0f30dadc9d
Uber Banquez <uberbanquezmartinez@gmail.com>
2026-04-23 23:40:10 -0500

    Clase libro y prestamos final

--- src/main/java/org/buber/modelos/Libro.java
+++ src/main/java/org/buber/modelos/Libro.java
@@ -1,4 +1,64 @@
 package org.buber.modelos;
 
 public class Libro {
+    private final String isbn;
+    private final String titulo;
+    private Autor autor;
+    private Categoria categoria;
+    private boolean disponible;
+
+    public Libro(String isbn, String titulo, Autor autor, Categoria categoria) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.disponible = true; // por defecto disponible
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void prestar() {
+        this.disponible = false;
+    }
+
+    public void devolver() {
+        this.disponible = true;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "isbn='" + isbn + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", autor=" + autor +
+                ", categoria=" + categoria +
+                ", disponible=" + disponible +
+                '}';
+    }
 }
--- src/main/java/org/buber/modelos/Prestamo.java
+++ src/main/java/org/buber/modelos/Prestamo.java
@@ -1,4 +1,70 @@
 package org.buber.modelos;
 
 public class Prestamo {
+    private final int id;
+    private Libro libro;
+    private String usuario;
+    private String fechaPrestamo;
+    private String fechaDevolucion;
+
+    public Prestamo(int id, Libro libro, String usuario, String fechaPrestamo, String fechaDevolucion) {
+        this.id = id;
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+
+        libro.prestar(); // el libro pasa a no disponible
+    }
+
+    public void devolverLibro() {
+        libro.devolver();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "id=" + id +
+                ", libro=" + libro +
+                ", usuario='" + usuario + '\'' +
+                ", fechaPrestamo='" + fechaPrestamo + '\'' +
+                ", fechaDevolucion='" + fechaDevolucion + '\'' +
+                '}';
+    }
 }
